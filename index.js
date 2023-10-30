e980977d36c1b3c07169ddd3c4f1d7decf5ab15c
Ibrahim Abdulbaqi <abdulbaqii403@gmail.com>
2023-10-30 12:13:28 +0100

    Correct some mistakes

---
+++ index.js
@@ -0,0 +1,130 @@
+
+
+// let x = 2;
+// let y = 1;
+// let z = x + y;
+// console.log(z);
+// const all = 3;
+
+
+// const num = "lorem";
+// console.log(num);
+// let f = "11";
+// let b = 1;
+// console.log(typeof(f));
+
+
+// const cars = ["saab","volvo","bmw","ff"];
+
+// cars.pop();
+// cars.push("first");
+// cars.unshift("toyota");
+// cars.shift();
+// console.log(cars);
+// let r = null;
+// console.log(r);
+// let fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.sort();   
+// console.log(fruits);
+
+
+// function baqi(year){
+//   if (year >= 2 && year <= 3) {
+//     return "Kindergerteen";
+//   }
+//   else if (year >= 4 && year <= 5) {
+//     return "Nursery";
+//   }
+//   else if (year >= 6 && year  <= 11){
+//     return "Primary";
+//   }
+//   else if (year >= 12 && year  <= 15){
+//     return "Jnr-Seconary";
+//   }
+//   else if (year >= 16 && year  <= 18){
+//     return "Snr-Sencodary";
+//   }
+//   else if (year >= 19 && year  <= 24){
+//     return "Medicine";
+//   }
+//   else if (year >= 25 && year  <= 29){
+//     return "Masters";
+//   }
+//   else {
+//     return "P.H.D";
+//   }
+// }
+// console.log(`//////////////////////////////////////////////////////`)
+// console.log(baqi(25))
+
+/*
+console.log('Abdulbaqi');
+
+let firstname='Baddest'
+console.log(firstname)
+console.log(firstname)
+console.log(firstname)
+console.log(firstname)
+*/
+
+//ADDITION
+let a = 30;
+let b = 10;
+let z = a + b;
+console.log(z)
+
+//SUBSTRACTION
+let d = 12;
+let s = 20;
+let q = d - s;
+console.log(q)
+
+//MULTIPLICATION
+let ram = 12;
+let man = 20;
+let Orange = man * ram;
+console.log(Orange)
+
+//EXPONENTATION
+let diamond = 12;
+let silver = 2;
+let gold = diamond ** silver;
+console.log(gold)
+
+
+//DIVISION
+let pacify = 80;
+let boss = 40;
+let owner = pacify / boss;
+console.log(owner)
+
+//MODULUS(REMAINDER)
+let baqi = 50;
+let baki = 130;
+let laptop = baki % baqi;
+console.log(laptop)
+
+//INCREMENT
+let bah = 60;
+bah++
+let you = 50;
+you++
+let me = bah + you;
+console.log(me)
+
+//DECREMENT
+let right = 21;
+right--
+right--
+right--
+right--
+right = right - 1
+console.log(right);
+let left = 21;
+left--
+let middle = left / right;
+console.log(middle)
+
+
+let welcome = 'greet'
+if (welcome = 'greet') alert('Hello World')
